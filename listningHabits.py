b9d78267ad29b07bab8fae0f91d9a485ee35d231
Siddhant356 <siddhantshukla356@gmail.com>
2020-06-11 23:01:33 +0530

    Print list of available tracks

--- listningHabits.py
+++ listningHabits.py
@@ -1,15 +1,45 @@
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
+from time import sleep, ctime
+from collections import namedtuple
+from threading import Thread
+from os.path import isfile
+import csv
 
-opts = Options()
-opts.set_headless()
-assert opts.headless
-browser = Chrome(options=opts)
-browser.get('https://duckduckgo.com')
-search_form = browser.find_element_by_id('search_form_input_homepage')
-search_form.send_keys('real pyton')
-search_form.submit()
-results = browser.find_elements_by_class_name('result')
-print(results[0].text)
-browser.close()
-quit()
+
+BANDCAMP_FRONTPAGE='https://bandcamp.com/'
+
+class BandLeader():
+    def __init__(self):
+        # Create a headless browser
+        opts = Options()
+        opts.headless=True     
+        self.browser = Chrome(options=opts)
+        self.browser.get(BANDCAMP_FRONTPAGE)
+
+        # Track list related state
+        self._current_track_number = 1
+        self.track_list = []
+        self.tracks()
+
+    def tracks(self):
+
+        # Sleep to give the browser time to render and finish any animations
+        sleep(1)
+
+        # Get the container for the visible track list
+        discover_section = self.browser.find_element_by_class_name('discover-results')
+        left_x = discover_section.location['x']
+        right_x = left_x + discover_section.size['width']
+
+        # Filter the items in the list to include only those we can click
+        discover_items = self.browser.find_element_by_class_name('discover-item')
+        self.track_list = [t for t in discover_items if t.location['x'] >= left_x and t.location['x'] < right_x]
+
+        # Print the available tracks to the screen
+        for (i,track) in enumerate(self.track_list):
+            print('[{}]'.format(i+1))
+            
+
+
+
